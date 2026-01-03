13350c7a2855dc1e89a780fefb550ceb372c5521
Bhaskar.Patra <bhaska.patra@gmail.com>
2026-01-03 20:51:38 +0530

    Controller added

---
+++ URLShortner/URLShortnerAPI/Controllers/ShortnerController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace URLShortnerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShortnerController : ControllerBase
+    {
+        public ShortnerController()
+        {
+        }
+
+        [HttpGet]
+        [Route("GetUrl")]
+        public IActionResult Get()
+        {
+            try
+            {
+                //Get the short Url from Database
+                return Ok("Short URL");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return BadRequest();
+        }
+
+
+
+
+        [HttpPost]
+        [Route("ShortenUrl")]
+        public IActionResult ShortenUrl(string longUrl)
+        {
+            try
+            {
+                //Generate a unique url in the Database and return it
+                return Ok("Hello");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return BadRequest();
+        }
+    }
+}
